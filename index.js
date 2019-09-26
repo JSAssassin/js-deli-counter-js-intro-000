588a9a9e8c9f6117ca9096cfaf5ce899b406d292
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2019-09-26 17:03:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,12 +14,14 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine(line) {
+  let newArray = [];
   if(line.length === 0) {
     return "The line is currently empty.";
   }
   else {
     for(let i = 0; i < line.length; i++) {
-      return `The line is currently: ${[i+1]}. ${line[i]}`
+      newArray.push(`${i+1}. ${line[i]}``);
     }
   }
+  return "The line is currently: " + newArray;
 }
