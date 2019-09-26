daf3eb4aacdd797deabcf2e95cc39befb078df39
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2019-09-26 17:10:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,10 +18,22 @@ function currentLine(line) {
   if(line.length === 0) {
     return "The line is currently empty.";
   }
-  else {
-    for(let i = 0; i < line.length; i++) {
-      newArray.push(`${i+1}. ${line[i]}`);
-    }
+  
+  for(let i = 0; i < line.length; i++) {
+    newArray.push(`${i+1}. ${line[i]}`);
   }
-  return "The line is currently: " + newArray.join(', ');
+  
+  return "The line is currently: " + newArray;
 }
+
+// function currentLine(line) {
+//    var lineNum = [];
+//    if (line.length == 0) {
+//      return "The line is currently empty.";
+//    }
+//    for (var i = 0; i < line.length; i++) {
+//      lineNum.push(` ${i+1}. ${line[i]}`);
+//  	}
+//    return "The line is currently:" + lineNum;
+// 
+//  }
