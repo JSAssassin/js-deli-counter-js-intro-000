e4215586f9e462942da5dd1be595231319a1860b
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2019-09-26 16:47:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function nowServing(katzDeliLine) {
   if(firstPerson === undefined) {
     return "There is nobody waiting to be served!";
   } else {
-    return "Currently serving" + firstPerson;
+    return "Currently serving " + firstPerson;
   }
 
   katzDeliLine.unshift();
