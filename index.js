6dba1814e6c2dcb2f7d2ea6b93866c72e48bd754
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2019-09-26 16:59:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,14 @@ function nowServing(katzDeliLine) {
     return "Currently serving " + firstPerson + '.';
   }
 }
+
+function currentLine(line) {
+  if(line.length === 0) {
+    return "The line is currently empty.";
+  }
+  else {
+    for(let i = 0; i < line.length; i++) {
+      return `The line is currently: ${[i+1]}. ${line[i]}`
+    }
+  }
+}
