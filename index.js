bc7041c425c3bfef84d14be414aac841d399e5a9
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2019-09-26 17:11:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,27 +13,27 @@ function nowServing(katzDeliLine) {
   }
 }
 
-function currentLine(line) {
-  let newArray = [];
-  if(line.length === 0) {
-    return "The line is currently empty.";
-  }
-
-  for(let i = 0; i < line.length; i++) {
-    newArray.push(` ${i+1}. ${line[i]}`);
-  }
-
-  return "The line is currently: " + newArray;
-}
-
 // function currentLine(line) {
-//    var lineNum = [];
-//    if (line.length == 0) {
-//      return "The line is currently empty.";
-//    }
-//    for (var i = 0; i < line.length; i++) {
-//      lineNum.push(` ${i+1}. ${line[i]}`);
-//  	}
-//    return "The line is currently:" + lineNum;
+//   let newArray = [];
+//   if(line.length === 0) {
+//     return "The line is currently empty.";
+//   }
 //
-//  }
+//   for(let i = 0; i < line.length; i++) {
+//     newArray.push(` ${i+1}. ${line[i]}`);
+//   }
+//
+//   return "The line is currently: " + newArray;
+// }
+
+function currentLine(line) {
+   var lineNum = [];
+   if (line.length == 0) {
+     return "The line is currently empty.";
+   }
+   for (var i = 0; i < line.length; i++) {
+     lineNum.push(` ${i+1}. ${line[i]}`);
+ 	}
+   return "The line is currently:" + lineNum;
+
+ }
