52f3407e8773d3e2b3de7719d4b465920dcfff1e
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2019-09-26 16:46:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,13 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.indexOf(name)+1} in line.`;
 }
+
+function nowServing(katzDeliLine) {
+  let firstPerson = katzDeliLine[0];
+  if(firstPerson === undefined) {
+    return "There is nobody waiting to be served!";
+  } else {
+    katzDeliLine.unshift();
+  }
+  return firstPerson;
+}
