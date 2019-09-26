50b9cb195234c7ebec698bff00ac20ed112f1244
Tashi D Gyeltshen <tashidgyeltshen@hotmail.com>
2019-09-26 16:39:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
-  return `Welcome, ${name}. You are number ${katzDeliLine.indexOf(name)} in line.`;
+  return `Welcome, ${name}. You are number ${katzDeliLine.indexOf(name)+1} in line.`;
 }
